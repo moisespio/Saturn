fa18e7c00c953f441690dcff5757a2b5909b0d71
Moisés Pio <moises@Pios-MacBook.local>
2015-06-22 10:03:10 -0300

    Initial Commit

---
+++ Saturn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Saturn
+//
+//  Created by Moisés Pio on 6/22/15.
+//  Copyright (c) 2015 Saturn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SaturnTests/SaturnTests.swift
@@ -0,0 +1,36 @@
+//
+//  SaturnTests.swift
+//  SaturnTests
+//
+//  Created by Moisés Pio on 6/22/15.
+//  Copyright (c) 2015 Saturn. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SaturnTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
