aee3dd658676e0ba971b50abd63437e08f385160
Moisés Pio <moises@Pios-MacBook.local>
2015-06-23 08:58:44 -0300

    Blurred Camera (in development)

--- Saturn/ViewController.swift
+++ Saturn/ViewController.swift
@@ -7,17 +7,69 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var blurredCameraUIView: UIView!
+
+    let captureSession = AVCaptureSession()
+    var previewLayer : AVCaptureVideoPreviewLayer?
+    var captureDevice : AVCaptureDevice?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        captureSession.sessionPreset = AVCaptureSessionPresetLow
+        let devices = AVCaptureDevice.devices()
+        
+        for device in devices {
+            if (device.hasMediaType(AVMediaTypeVideo)) {
+                if(device.position == AVCaptureDevicePosition.Back) {
+                    captureDevice = device as? AVCaptureDevice
+                    
+                    if captureDevice != nil {
+                        beginSession()
+                    }
+                }
+            }
+        }
+        
     }
+    
+    func configureDevice() {
+        if let device = captureDevice {
+            device.lockForConfiguration(nil)
+            device.focusMode = .Locked
+            device.unlockForConfiguration()
+        }
+        
+    }
+    
+    func beginSession() {
+        configureDevice()
+        
+        var err : NSError? = nil
+        captureSession.addInput(AVCaptureDeviceInput(device: captureDevice, error: &err))
+        
+        if err != nil {
+            println("error: \(err?.localizedDescription)")
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        blurredCameraUIView.layer.addSublayer(previewLayer)
+        blurredCameraUIView.layer.frame.size.width = blurredCameraUIView.layer.frame.size.width
+        blurredCameraUIView.layer.frame.size.height = blurredCameraUIView.layer.frame.size.height
+        blurredCameraUIView.layer.position.x = CGFloat(50)
+        blurredCameraUIView.layer.position.y = CGFloat(0)
+        
+        previewLayer?.frame = blurredCameraUIView.layer.frame
+        captureSession.startRunning()
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
