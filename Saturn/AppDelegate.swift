915e1ec3d8e646f63543dcc9c4a1ee3852ab2269
Luis Henrique Grassi <lhmgrassi@gmail.com>
2015-06-24 10:59:12 -0300

    appearance to navigationbar

--- Saturn/AppDelegate.swift
+++ Saturn/AppDelegate.swift
@@ -19,14 +19,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
-        var navigationBarAppearace = UINavigationBar.appearance()
-        navigationBarAppearace.barTintColor = UIColor(red: 127/255, green: 148/255, blue: 255/255, alpha: 1.0)
+        var navigationBarAppearance = UINavigationBar.appearance()
+        navigationBarAppearance.barTintColor = UIColor(red: 127/255, green: 148/255, blue: 255/255, alpha: 1.0)
         
         let lblSaturn : UILabel = UILabel(frame: CGRectMake(26, 27, 100, 70))
         lblSaturn.text = "SATURN"
         lblSaturn.backgroundColor = UIColor.redColor()
         
-        navigationBarAppearace.addSubview(lblSaturn)
+        navigationBarAppearance.addSubview(lblSaturn)
         
         Parse.enableLocalDatastore()
         
