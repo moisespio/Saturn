86b5e4bd3fbc790bd0e4a42a3b33d2a5ac5f793c
Rodrigo Lungui <lungui.rodrigo@gmail.com>
2015-06-25 09:51:59 -0300

    Initial commit to SensorViewController

---
+++ Saturn/SensorViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SensorViewController.swift
+//  Saturn
+//
+//  Created by Rodrigo Lungui on 25/06/15.
+//  Copyright (c) 2015 Saturn. All rights reserved.
+//
+
+import UIKit
+
+class SensorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
