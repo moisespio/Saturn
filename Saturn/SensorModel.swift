b26517af5826e40f01fd27d108d241db1a8a71a3
Luis Henrique Grassi <lhmgrassi@gmail.com>
2015-06-26 10:15:29 -0300

    added getSensors

--- Saturn/SensorModel.swift
+++ Saturn/SensorModel.swift
@@ -11,6 +11,14 @@ import Parse
 
 class SensorModel: NSObject {
     
+    init(sensorParseObject : PFObject)
+    {
+        self.sensorParseObject = sensorParseObject
+        self.sensorCode = sensorParseObject["code"] as? String
+        self.sensorName = sensorParseObject["name"] as? String
+        self.sensorDescription = sensorParseObject["description"] as? String
+    }
+    
     var sensorParseObject : PFObject?
     var sensorCode : String?
     var sensorName : String?
@@ -49,6 +57,38 @@ class SensorModel: NSObject {
                 function(false, error!.description)
             }
         }
+    }
+    
+    ///
+    /// Usage:
+    ///         SensorModel.getSensors { 
+    ///             (sensorList: Array<SensorModel>?) -> Void in
+    ///                 if (sensorList != nil) {
+    ///                     //your code here
+    ///                 }
+    ///        }
+    static func getSensors(function: (Array<SensorModel>?) -> Void) -> Void
+    {
+        var query = PFQuery(className:"Sensor")
+        query.whereKey("installation", equalTo:PFInstallation.currentInstallation().objectId!)
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]?, error: NSError?) -> Void in
+            var sensorList = Array<SensorModel>()
+            if (error == nil) {
+                println("Successfully retrieved \(objects!.count) sensors.")
+                if let sensors = objects as? [PFObject] {
+                    for sensor in sensors {
+                        let sensorModel = SensorModel(sensorParseObject: sensor)
+                        sensorList.append(sensorModel)
+                    }
+                }
+                function(sensorList)
+            } else {
+                println("Error: \(error!) \(error!.userInfo!)")
+                function(nil)
+            }
+        }
+        
     }
     
 }
