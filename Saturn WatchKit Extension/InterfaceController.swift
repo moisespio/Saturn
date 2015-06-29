648ebf34d4a6e0edae573039aadbdccf7b71329e
Luis Henrique Grassi <lhmgrassi@gmail.com>
2015-06-29 11:07:34 -0300

    Addes watchkit

---
+++ Saturn WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Saturn WatchKit Extension
+//
+//  Created by Luis Henrique on 6/29/15.
+//  Copyright (c) 2015 Saturn. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
